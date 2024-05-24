c5747d82a2eeefdc7dfc11b5e96e62851248c301
dap2468 <34717530+dap2468@users.noreply.github.com>
2024-05-24 05:11:01 -0500

    Add files via upload

---
+++ About Project/about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Project</title>
+    <link rel="stylesheet" href="about.css">
+  </head>
+
+  <body>
+    <form>
+      <h1> About Project</h1>
+
+      <p class="cat">Project Title : Asset Security and Inventory Using IoT</p>
+      <p class="cat">Student Name: Daniel Palmer</p>
+      <p class="cat">Project Proposer : Lindon Falconer</p>
+      <p class="cat">Project Supervisor : Lindon Falconer</p>
+      <p class="cat">Project Category : Type IV: Investigation with System Development and/or Design</p>
+      <p class="cat">Thematic Group : Electronics, Computer Systems, Communications</p>
+      <p class="cat">Project Keywords : Wireless Communication, Email, IoT</p>
+
+      
+      
+      <p class="subtitle">Objectives</p>
+      <p class="info">
+        1.	Design and develop a battery-operated wireless monitoring system (WMS) which can continuously communicate with a server.
+        The WMS should send an alert message to the server if movement or power disconnect is detected. 
+      </p>
+      <p class="info">
+        2.	The WMS should be small enough to be placed in laboratory equipment such as an oscilloscope, power supply or function generator. 
+        The battery in the WMS should charge when the equipment is plugged into the mains.
+      </p>
+      <p class="info">
+        3.	Design and implement a web server to communicate with multiple WMS.  The web server should link a WMS ID to a specific equipment.  
+        The web server should show if the equipment is online, offline, plugged in or out, moving or not moving.
+      </p>
+      <p class="info">
+        4.	The web server should send an email alert and enable an alarm if equipment is moved. 
+      </p>
+      <p class="info">5.	The web server application should be designed to add, edit or delete a WMS.</p>
+      <p class="info">6.	The web server application should have a table that shows the inventory of the equipment being monitored.</p>
+
+      <button type="submit" formaction="http://localhost/First%20Screen/first.php" class="home">Back</button>
+
+    </form>
+  </body>
+
+</html>
+
